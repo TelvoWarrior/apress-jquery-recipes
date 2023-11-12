b1d81ce786d1b3ac60849af9f835174f5ae15239
Telvo <telvowarrior@gmail.com>
2023-11-13 02:09:12 +0300

    do a context menu on right click, stuck

---
+++ 5-5_Creating-a-Context-Menu-on-Right-Click/index.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    //После загрузки страницы контекстное меню скрывается
+    $("#contextmenu").hide();
+    
+    //При нажатии правой кнопкой мыши контекстное меню появляется
+    //Указывается позиция на которой появится меню
+    $(".info").mousedown(function(event){
+        if(event.button==2){
+            $("#contextmenu").show();
+            $("#contextmenu").css({"position":"absolute","left":event.screenX,"top":event.screenY-70});
+        }
+    });
+    
+    //Изменение фона меню при наведении на него указателя мыши
+    //Возвращаение к стандартному виду, если указатель убрать
+    $("a").on("mouseenter",function(){
+        $(this).addClass("hover");
+    }).on("mouseleave",function(){
+        $(this).removeClass("hover");
+    });
+    
+    //Данный код должен убирать контекстное меню при нажатии клавиши ESC, но этого не происходит
+    $("body").on("keypress",function(event){
+        if(event.key="Escape") {
+            $("#contextmenu").hide();
+        }
+    });
+});
