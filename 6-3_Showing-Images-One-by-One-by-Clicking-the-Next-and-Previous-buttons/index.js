f1fdcd050e4de42a8a387232c8b00d809f366e94
Telvo <telvowarrior@gmail.com>
2023-11-17 01:09:20 +0300

    it became clear to me, most difficult task so far

--- 6-3_Showing-Images-One-by-One-by-Clicking-the-Next-and-Previous-buttons/index.js
+++ 6-3_Showing-Images-One-by-One-by-Clicking-the-Next-and-Previous-buttons/index.js
@@ -1,2 +1,20 @@
 $(document).ready(function(){
+    $("img.left-arrow").click(function(){
+        if($("ul").css("marginLeft")=="0px"){
+            alert("You are at the first image.");
+        } else {
+            $("#image-slider").children("ul").animate({
+                "marginLeft":"+=1000px"
+            },1000);
+        };
+    });
+    $("img.right-arrow").click(function(){
+        if($("ul").css("marginLeft")=="-4000px"){
+            alert("You are at the last image");
+        } else {
+            $("#image-slider").children("ul").animate({
+                "marginLeft":"-=1000px"
+            },1000);
+        };
+    });
 });
