ed4f2a6f0906d6889bd7de60397a98aa69cd58b9
Telvo <telvowarrior@gmail.com>
2023-11-16 11:42:15 +0300

    section done, didn't do any changes, a little disappointed with final result

--- 5-9_Making-a-Dynamic-Visual-Menu/index.js
+++ 5-9_Making-a-Dynamic-Visual-Menu/index.js
@@ -1,2 +1,34 @@
 $(document).ready(function(){
+    $(".books").hide();
+    $(".movies").hide();
+    $(".music").hide();
+
+    $("a").hover(
+        function(event){
+            $(this).addClass("rightselectfig");
+            $(this).parent().addClass("leftselectfig");
+        },
+        function(){
+            $(this).removeClass("rightselectfig");
+            $(this).parent().removeClass("leftselectfig");
+        }
+    );
+    $("#booksbutton").click(function(event){
+        event.preventDefault();
+        $(".books").slideDown("slow");
+        $(".movies").slideUp("show");
+        $(".music").slideUp("show");
+    });
+    $("#moviesbutton").click(function(event){
+        event.preventDefault();
+        $(".books").slideUp("show");
+        $(".movies").slideDown("slow");
+        $(".music").slideUp("show");
+    });
+    $("#musicbutton").click(function(event){
+        event.preventDefault();
+        $(".books").slideUp("show");
+        $(".movies").slideUp("show");
+        $(".music").slideDown("slow");
+    });
 });
